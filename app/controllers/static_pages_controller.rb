9e1f2449215d669d3b5132dee0e5cb6784bad667
Anfal Abdul Jaleel <anfalaj@gmail.com>
2013-08-15 14:37:46 -0700

    Add a full_title helper

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,5 +6,10 @@ class StaticPagesController < ApplicationController
   end
   
   def about
+  @recipe = {"Ingredients" => ["tilapia", "salt", "chilli", "coconut oil"], "Steps" => ["defrost fist", "wipe dry", "sprinkle salt and chili", "heat pan","add oil", "fry till brown on each side"]}
   end
+  
+  def contact
+  end
+  
 end
