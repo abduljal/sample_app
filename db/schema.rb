e047cc9e0d5d0c164187031d87cb67e071b0cf7a
Anfal Abdul Jaleel <anfalaj@gmail.com>
2013-08-16 13:40:47 -0700

    midway

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130816012142) do
+ActiveRecord::Schema.define(version: 20130816200700) do
+
+  create_table "ingredients", force: true do |t|
+    t.string   "name"
+    t.string   "amount"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "recipes", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "steps", force: true do |t|
+    t.string   "process"
+    t.integer  "order"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
